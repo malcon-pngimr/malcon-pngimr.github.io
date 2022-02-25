ff8050d7ea1f6035d7ddc3956399e64996985469
malcon-pngimr <100398964+malcon-pngimr@users.noreply.github.com>
2022-02-25 14:55:31 +1000

    Create render_index.R

---
+++ scripts/render_index.R
@@ -0,0 +1,12 @@
+
+# Landing Page ------------------------------------------------------------
+
+# install postcards to create landing page
+install.packages(c("postcards"))
+
+# render landing page
+rmarkdown::render("docs/index.Rmd")
+
+
+
+# Blog --------------------------------------------------------------------
